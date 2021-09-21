e61e422d198d9a090305bcc371c257f6ed347e24
Achille Astolfi <achille.astolfi@infocommproject.it>
2021-09-21 12:23:03 +0200

    Lato core terminato

--- src/main/java/com/example/library/app/book/BookMapper.java
+++ src/main/java/com/example/library/app/book/BookMapper.java
@@ -1,12 +1,31 @@
 package com.example.library.app.book;
 
 import com.example.library.app.author.Author;
+import com.example.library.model.book.BookCreateDto;
 import com.example.library.model.book.BookResource;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
+    @Mapping(target = "id", ignore = true)
+    // trasferiamo authorId del Dto nel campo id dell'author dell'entity
+    // nel caso complesso, abbiamo bisogno di un mapper più completo che mi vada
+    // a prendere i valori di firstName e lastName dall'AuthorCreateDto
+    @Mapping(target = "author", source = "authorId")
+    Book toEntity(BookCreateDto createDto);
+
+    // da capire perché con una build incrementale questo metodo viene generato
+    // in automatico mentre con una rebuild questo metodo non viene generato
+    default Author map(Long authorId) {
+        if (authorId == null) {
+            return null;
+        }
+        return Author.builder()
+                .id(authorId)
+                .build();
+    }
+
     // per tutte le property che non voglio mappare ho la possibilità di dichiarare una annotation
     // sul metodo
     @Mapping(target = "cavallo", ignore = true)
--- src/main/java/com/example/library/app/book/BookServiceImpl.java
+++ src/main/java/com/example/library/app/book/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.library.app.book;
 
 import com.example.library.app.author.Author;
 import com.example.library.app.author.AuthorRepository;
+import com.example.library.model.book.BookCreateDto;
 import com.example.library.model.book.BookResource;
 import com.example.library.model.book.BookService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
@@ -58,6 +60,35 @@ public class BookServiceImpl implements BookService {
         Optional<Book> maybeBook = this.bookRepository.findById(bookId);
         // maybeBook ha un metodo map che prende come argomento un method reference
         return maybeBook.map(this::toResource);
+    }
+
+    @Override
+    @Transactional
+    public BookResource createBook(BookCreateDto dto) {
+        // abbiamo i metodi e abbiamo le dipendenze
+        Book book = this.bookMapper.toEntity(dto);
+        // MA...
+        // quando si lavora con JPA le cose devono essere fatte con cautela
+        // Il mapper crea (new) un oggetto Author che però riferisce un
+        // record di database già esistente; questo non va bene, perché
+        // se il record di database esiste, l'oggetto Author deve essere
+        // creato (new) da Hibernate (o chi per esso).
+        // NOTA BENE: a noi non interessa fare davvero la select sulla tabella author
+        // a noi interessa avere l'oggetto gestito da Hibernate (o...)
+        Long idEsistente = book.getAuthor().getId();
+        // NB: nel caso complicato: se idEsistente == null allora creo un nuovo author
+        //     altrimenti procedo come nel resto di questo metodo
+        Author esistente = this.authorRepository.getOne(idEsistente);
+        // se Hibernate ha già creato l'oggetto Author corrispondente all'id, mi restituisce l'oggetto
+        // se invece non l'ha creato, Hibernate restituisce un "proxy" dell'oggetto; solo quando
+        // per esempio faremo esistente.getFirstName() allora Hibernate eseguirà la SELECT sul DB
+        // Il risultato è che in nessuno dei due casi il metodo getOne fa SELECT sul DB
+        // esistente è diverso da book.getAuthor(), quindi lo devo riassegnare
+        book.setAuthor(esistente);
+        // ricordarsi di riassegnare la variabile
+        book = this.bookRepository.save(book);
+        // adesso posso trasformare la entity in resource
+        return this.bookMapper.toResource(book);
     }
 
     // per semplificarci la vita scriviamo un metodo che ha come parametro Book
