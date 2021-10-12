e36a4ba72700b5d3488b438673eccb7df1c7ca8b
Achille Astolfi <achille.astolfi@infocommproject.it>
2021-10-12 20:54:49 +0200

    Introduco MySQL

--- src/main/java/com/example/library/app/book/BookServiceImpl.java
+++ src/main/java/com/example/library/app/book/BookServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
@@ -20,38 +19,6 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     // aggiungo nelle dipendenze il mapper di mapStruct
     private final BookMapper bookMapper;
-
-    // usiamo un metodo per caricare dei dati finti nel DB, tanto ogni volta vengono azzerati
-    // Il metodo può essere private "package protected" protected o public
-    // Lo annoto con PostConstruct; viene invocato da Spring dopo il costruttore e dopo aver
-    // risolto tutte le dipendenze
-    @PostConstruct
-    private void caricaDatiFinti() {
-        Author king = Author.builder()
-                .lastName("King")
-                .firstName("Stephen")
-                .build();
-        Author rowling = Author.builder()
-                .lastName("Rowling")
-                .firstName("J. K.")
-                .build();
-        // per inserire o aggiornare un record di DB si usa il metodo save del repository
-        // questo metodo è disponibile in tutte le implementazioni "CRUD" di Spring Data
-        // NOTA BENE: è molto importante riassegnare la variabile quando si invoca il metodo
-        // save; nella maggioranza dei casi è lo stesso oggetto ma POTREBBE essere diverso
-        king = this.authorRepository.save(king);
-        rowling = this.authorRepository.save(rowling);
-
-        Book it = new Book();
-        it.setTitle("It");
-        it.setAuthor(king);
-        Book potter = new Book();
-        potter.setTitle("Harry Potter e la pietra filosofale");
-        potter.setAuthor(rowling);
-
-        it = this.bookRepository.save(it);
-        potter = this.bookRepository.save(potter);
-    }
 
     @Override
     public Optional<BookResource> readBookById(Long bookId) {
