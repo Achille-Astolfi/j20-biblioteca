1c5ce87f5c856ff471cb0fee532d9229c4e8e2e8
Michela Garau <michela.garau@realt.it>
2021-10-13 10:57:07 +0200

    test author + book

--- src/test/java/com/example/library/model/author/AuthorServiceTest.java
+++ src/test/java/com/example/library/model/author/AuthorServiceTest.java
@@ -5,13 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.example.library.app.LibraryBoot;
 
 import java.util.Optional;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = LibraryBoot.class)
 @ActiveProfiles("jupiter")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AuthorServiceTest {
   // intreccio Jupiter con spring framework in due modi
   // 1. dichiarando la dipendenza del component che è la mia unit
@@ -19,6 +25,7 @@ class AuthorServiceTest {
   @Autowired
   private AuthorService authorService;
   @Test
+  @Order(1)
   void readAuthorByIdTest() {
     Long inputId = 1L;
     Optional<AuthorResource> output = this.authorService.readAuthorById((inputId));
@@ -36,9 +43,36 @@ class AuthorServiceTest {
 
   @Test
   void readAuthorByIdNullArgumentTest() {
+    // l'obbietivo del test è evocare readAuthorById passando l'argomento null
+    // e verificare che venga sollevata l'eccezione attesa
+    // si tratta di un'eccezione di Spring Data,
+    // perchè tutte le ecceioni sollevate dalle class annotate con @Repository
+    // vengono incapsulate in queste eccezioni di Spring data
+    // Il metodo per intercettare le ecceizoni attese è aassertThrows che vuole
+    // come primo argomento la class dell'eccezione da verificare
+    // e come secondo argomento vuole una "lambda" o un "method referece"
+    // Scriviamo un metodo private dentro questa class di test con il test
+    // da eseguire e che vogliamo solevi l'eccezione
+    // Ho bisogno di verficare che l'eccezione sollevata abbia come cause illegalArgmentException
+    // devo invocare readAuthorByIdNullArgumentImpl() non direttamente ma come method reference
+    // Interpreto il method refrence non come invocazione istantanea ma come invocazione differita
+
+    RuntimeException exception = assertThrows(RuntimeException.class, this::readAuthorByIdNullArgumentImpl);
+    // i più pignoli possono mettere un'assert che getcause() sia notNull
+    assertNotNull(exception.getCause());
+    // verifico che la cause sa il tipo IllegalArgumentException
+    assertEquals(IllegalArgumentException.class, exception.getCause().getClass());
+
+
+  }
+  private void readAuthorByIdNullArgumentImpl() {
+    // questo è il test che voglio eseguire nel metodo readAuthorByIdNullArgumentTest()
+    // e che voglio sollevi una eccezione
+    this.authorService.readAuthorById(null);
   }
 
   @Test
+  @Order(0)
   void createAuthorTest() {
     AuthorCreateDto inputDto = new AuthorCreateDto();
     inputDto.setFirstName("Grazia");
@@ -52,6 +86,24 @@ class AuthorServiceTest {
   }
 
   @Test
-  void createAuthornoNameTest() {
+  void createAuthorNoNameTest() {
+    assertThrows(DataIntegrityViolationException.class, this::createAuthorNoNameImpl);
+  }
+  private void createAuthorNoNameImpl() {
+    // crea un DTO ma non imposto ne nome ne cognome
+    AuthorCreateDto dto = new AuthorCreateDto();
+    this.authorService.createAuthor(dto);
+  }
+
+  @Test
+  void createAuthorNullArgumentTest() {
+    // come sopra readAuthorByIdNullArgumenttest
+    InvalidDataAccessApiUsageException exception = assertThrows(InvalidDataAccessApiUsageException.class, this::createAuthorNullArgumentImpl);
+    assertNotNull(exception.getCause());
+    assertEquals(IllegalArgumentException.class, exception.getCause().getClass());
+  }
+
+  private void createAuthorNullArgumentImpl() {
+    this.authorService.createAuthor(null);
   }
 }
--- src/test/java/com/example/library/model/book/BookServiceTest.java
+++ src/test/java/com/example/library/model/book/BookServiceTest.java
@@ -3,21 +3,34 @@ package com.example.library.model.book;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.library.app.LibraryBoot;
+import com.example.library.app.author.Author;
+import com.example.library.app.author.AuthorRepository;
 import com.example.library.app.book.BookMapper;
 import java.util.Optional;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = LibraryBoot.class)
 @ActiveProfiles("jupiter")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BookServiceTest {
   @Autowired
   private BookService bookService;
   // se non creiamo l'implementazione farlocca di Bookmapper
   // la nostra unit è in realtà BookServiceImpl + BookMapperImpl
+
+  @Autowired
+  private AuthorRepository authorRepository;
+
   @Test
+  @Order(1)
   void readBookByIdTest() {
     Long inputId = 1L;
     Optional<BookResource> output = this.bookService.readBookById((inputId));
@@ -34,14 +47,44 @@ class BookServiceTest {
   }
 
   @Test
+  @Order(0)
   void createBookTest() {
+    //sono "obbligato" a creare un record dentro la tabella author altrimenti il test non può essere eseguito
+    Author author = new Author();
+    author.setLastName("Rowling");
+    this.authorRepository.save(author);
+
+    BookCreateDto inputDto = new BookCreateDto();
+    inputDto.setTitle("Harry Potter e il secondo volume");
+    inputDto.setAuthorId(1L);
+    BookResource output = this.bookService.createBook(inputDto);
+    assertNotNull(output);
+    assertEquals(inputDto.getTitle(), output.getTitle());
+    assertNotNull(output.getAuthor());
   }
 
   @Test
+  @Order(1) //perchè ho bisogno di avere un record dentro la tabella author, questo record è creato nel method crateBooktest
   void createBookNoTitleTest() {
+    assertThrows(DataIntegrityViolationException.class, this::createBookNoTitleTestImpl);
+    // creaiamo un DTO con l'autore senza il titolo(mi aspetto dataIntegrityViolation)
+  }
+
+  private void createBookNoTitleTestImpl() {
+    BookCreateDto inputDto = new BookCreateDto();
+    inputDto.setAuthorId(1L);
+    this.bookService.createBook(inputDto);
   }
 
   @Test
   void createBookNoAuthorTest() {
+  // creaiamo un DTO sena l'autore con il titolo(mi aspetto dataIntegrityViolation)c
+    assertThrows(InvalidDataAccessApiUsageException.class, this::createBookNoAuthorImpl);
+  }
+  private void createBookNoAuthorImpl() {
+    //mi aspetto un null pointer
+    BookCreateDto inputDto = new BookCreateDto();
+    inputDto.setTitle("Nuovo libro");
+    this.bookService.createBook(inputDto);
   }
 }
